fdb7f29a5dc4a077a8dc806bb6936b2728b050f6
Bia <82560900+BiatrizR@users.noreply.github.com>
2023-02-16 17:16:21 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var balloon,balloonImage1,balloonImage2;
-var database;
-var height;
+
+
 
 function preload(){
    bg =loadImage("Images/cityImage.png");
@@ -10,17 +10,19 @@ function preload(){
    "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
   }
 
-//Função para definir o ambiente inicial
+
 function setup() {
-  database=firebase.database();
+ //chamar db aqui
+   
+   
   createCanvas(1500,700);
 
   balloon=createSprite(250,650,250,650);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
-  var balloonHeight=database.ref('balloon/height');
-  balloonHeight.on("value",readHeight, showError);
+  //bd
+   
   textSize(20); 
 }
 
@@ -29,20 +31,23 @@ function draw() {
   background(bg);
 
   if(keyDown(LEFT_ARROW)){
-    updateHeight(-10,0);
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
+   //imagem e direção do movimento
+
   }
+   
   else if(keyDown(RIGHT_ARROW)){
-    updateHeight(10,0);
+  //
     balloon.addAnimation("hotAirBalloon",balloonImage2);
   }
+   
   else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10);
+   //
     balloon.scale=balloon.scale -0.005;
     balloon.addAnimation("hotAirBalloon",balloonImage2);
   }
+   
   else if(keyDown(DOWN_ARROW)){
-    updateHeight(0,+10);
+
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale=balloon.scale+0.005;
   }
@@ -56,19 +61,11 @@ function draw() {
 }
 
 
-function updateHeight(x,y){
-  database.ref('balloon/height').update({
-    'x': height.x + x ,
-    'y': height.y + y
-  })
-}
+//função updateHeight
 
 
 function readHeight(data){
-  height = data.val();
-  console.log(height.x);
-  balloon.x = height.x;
-  balloon.y = height.y;
+ // adicionar propriedades dessa função
  }
 
 function showError(){
